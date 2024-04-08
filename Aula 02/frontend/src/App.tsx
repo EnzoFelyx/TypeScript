365b2a8107c1f2f59cc9fa4506c8318fb29dda72
EnzoFelyx <enzofelyx@gmail.com>
2024-04-07 22:50:59 -0300

    finished

--- Aula 02/frontend/src/App.tsx
+++ Aula 02/frontend/src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api'
+
+import User from './components/User'
+
+interface IUser {
+  name: string;
+  email: string;
+}
 
 function App() {
+
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    api.get<IUser[]>('/users').then(response => {
+      setUsers(response.data);
+    })
+  })
+
   return (
     <div className="App">
-      
+      {users.map(users => <User key={users.email} user={users} />)}
     </div>
   );
 }
